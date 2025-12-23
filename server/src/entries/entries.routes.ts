48bd157e06c66b928b28c2503c2590433acdb4f3
alejandro <alejandrosotonmd@gmail.com>
2025-12-23 12:36:20 -0400

    feat(entries): expose entry routes for the API

---
+++ server/src/entries/entries.routes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createEntry, deleteEntryById, getAllEntries, getEntryByEntryId, getPatientEntries, updateEntryController } from './entries.controller';
+
+const router = express.Router();
+
+router.get('/', getAllEntries);
+router.get('/patientId/:id', getPatientEntries)
+router.get('/patientId/:id/entryId/:id', getEntryByEntryId);
+router.post('/', createEntry);
+router.delete('/patientId/:id', deleteEntryById);
+router.put('/patientId/:patientId/entryId/:entryId', updateEntryController);
+
+export default router;
