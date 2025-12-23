5dacd6dcffc7ffb83179c1e6819f6272dc49b01c
alejandro <alejandrosotonmd@gmail.com>
2025-12-23 16:17:20 -0400

    feat(auth): add auth routes for admin login

---
+++ server/src/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { login } from "../auth/auth.controller";
+
+const router = express.Router();
+
+// POST /auth/login
+router.post("/login", login);
+
+export default router;
