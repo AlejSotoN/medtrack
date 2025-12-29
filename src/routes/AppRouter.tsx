7715ad595e203b8efd96c5f4d9d41763e0db423f
alejandro <alejandrosotonmd@gmail.com>
2025-12-29 00:41:22 -0400

    refactor(router): reorganize routes with layouts and protection

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,23 +1,25 @@
 import { createBrowserRouter } from "react-router-dom"
 import Dashboard, { loader as dashboardLoader } from "../features/Dashboard/Dashboard"
-import PatientProfile from "../features/PatientProfile/PatientProfile"
 import { patientLoader, patientProfileLoader } from "../features/PatientProfile/loader"
-import Entries from "../features/Entries/Entries"
-import Home from "../features/Home/Home"
-import Auth from "../features/Auth/Auth"
-import Root from "../layout/Root/Root"
-import Logged from "../layout/Logged/Logged"
 import { getPatient, getPatients } from "../services/patient.client"
-import NotFound from "../components/ui/NotFound/NotFound"
 import { entriesLoader } from "../features/Entries/loader"
 import { getEntries, getEntriesByPatientId } from "../services/entries.client"
+import Logged from "../layout/Logged/Logged"
+import PublicLayout from "../layout/PublicLayout/PublicLayout"
+import AuthLayout from "../layout/AuthLayout/AuthLayout"
+import ProtectedRoute from "./ProtectedRoute"
+import NotFound from "../components/ui/NotFound/NotFound"
+import Home from "../features/Home/Home"
+import Auth from "../features/Auth/Auth"
+import PatientProfile from "../features/PatientProfile/PatientProfile"
+import Entries from "../features/Entries/Entries"
 import EditEntry from "../features/Entries/EditEntry"
 import editPatient from "../features/editPatient/editPatient"
 
 export const AppRouter = createBrowserRouter([
     {
         path: "/",
-        Component: Root,
+        Component: PublicLayout,
         ErrorBoundary: NotFound,
         children: [
             {
@@ -28,38 +30,48 @@ export const AppRouter = createBrowserRouter([
     },
     {
         path: "login",
-        Component: Auth,
-    },
-    {
-        Component: Logged,
-        ErrorBoundary: NotFound,
+        Component: AuthLayout,
         children: [
             {
-                path: "dashboard",
-                Component: Dashboard,
-                loader: dashboardLoader(getPatients)
-            },
-            {
-                path: "dashboard/patient/:patientId",
-                Component: PatientProfile,
-                loader: patientProfileLoader(getPatient, getEntriesByPatientId),
-            },
-            {
-                path: "dashboard/patient/edit/:patientId",
-                Component: editPatient,
-                loader: patientLoader(getPatient),
-            },
-            {
-                path: "/dashboard/patient/:patientId/edit-entry/:entryId",
-                Component: EditEntry,
-                loader: entriesLoader(getEntries)
-            },
-            {
-                path: "/entries",
-                Component: Entries,
-                loader: entriesLoader(getEntries)
+                index: true,
+                Component: Auth,
             },
         ]
     },
-
+    {
+        Component: ProtectedRoute,
+        children: [
+            {
+                Component: Logged,
+                ErrorBoundary: NotFound,
+                children: [
+                    {
+                        path: "dashboard",
+                        Component: Dashboard,
+                        loader: dashboardLoader(getPatients)
+                    },
+                    {
+                        path: "dashboard/patient/:patientId",
+                        Component: PatientProfile,
+                        loader: patientProfileLoader(getPatient, getEntriesByPatientId),
+                    },
+                    {
+                        path: "dashboard/patient/edit/:patientId",
+                        Component: editPatient,
+                        loader: patientLoader(getPatient),
+                    },
+                    {
+                        path: "dashboard/patient/:patientId/edit-entry/:entryId",
+                        Component: EditEntry,
+                        loader: entriesLoader(getEntries)
+                    },
+                    {
+                        path: "entries",
+                        Component: Entries,
+                        loader: entriesLoader(getEntries)
+                    },
+                ]
+            }
+        ]
+    },
 ])
