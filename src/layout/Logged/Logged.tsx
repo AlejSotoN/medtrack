3af476166357dd110a4a06ab1a985d917a6ca859
alejandro <alejandrosotonmd@gmail.com>
2025-12-29 00:41:35 -0400

    feat(auth): implement logout action in logged layout

--- src/layout/Logged/Logged.tsx
+++ src/layout/Logged/Logged.tsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import Navbar, { NavbarTab } from '../../components/ui/Navbar/Navbar'
-import { Outlet, useLocation, useNavigate, useNavigation } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import styles from './Logged.module.css'
+import { logout } from '../../services/auth.client';
 
 export default function Logged() {
     const location = useLocation();
     const navigate = useNavigate();
-    const navigation = useNavigation()
 
     const currentRoute = location.pathname;
-    
+
     const handleNavigation = (route: string) => {
+        if (route === '/logout') {
+            logout();
+            navigate('/login', { replace: true });
+            return;
+        }
+
         navigate(route);
     }
 
     const loggedNavbarTabs: NavbarTab[] = [
         { label: 'Dashboard', route: '/dashboard', icon: '🏠' },
         { label: 'Entries', route: '/entries', icon: '🔧' },
-        { label: 'Logout', route: '/', icon: '🔧' },
-      
+        { label: 'Logout', route: '/logout', icon: '🔧' },
+
     ];
-    
+
     return (
-    <div className={styles.div}>
-        <Navbar
-            tabs={loggedNavbarTabs}
-            onTabClick={handleNavigation}
-            activeRoute={currentRoute}
-        />
-        <Outlet/>
-    </div> 
+        <div className={styles.div}>
+            <Navbar
+                tabs={loggedNavbarTabs}
+                onTabClick={handleNavigation}
+                activeRoute={currentRoute}
+            />
+            <Outlet />
+        </div>
     )
 }
