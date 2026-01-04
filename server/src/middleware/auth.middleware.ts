3bd48bc357fa9922d4294774009c55a50b2c22cf
alejandro <alejandrosotonmd@gmail.com>
2026-01-04 00:48:20 -0400

    feat(auth): add JWT auth middleware and typed request user

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 interface JwtPayload {
+    sub: string;
     role: string;
 }
 export interface AuthenticatedRequest extends Request {
